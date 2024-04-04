13c9f93a4ee02ea3be205a6d0d349b1ab8ba15e9
Irina Ichim <onlyirina7@gmail.com>
2024-04-04 02:00:06 +0200

    primera prueba realizada

--- prueba/script.js
+++ prueba/script.js
@@ -1,23 +1,62 @@
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-        createFireworks();
-    }, 1000);
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    createFireworks();
+    typeWriterEffect("¡Gracias por ser parte de nuestros 1000 seguidores!", [
+      "#4737BB",
+      "#FFFFFF",
+      "#FF5A02",
+    ]);
+  }, 1000);
 });
 
 function createFireworks() {
-    for (let i = 0; i < 50; i++) {
-        createFirework();
+  const colors = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "purple",
+    "orange",
+    "indigo",
+    "cyan",
+    "magenta",
+    "turquoise",
+    "pink",
+    "lime",
+  ];
+
+  for (let i = 0; i < 50; i++) {
+    createFirework(colors[Math.floor(Math.random() * colors.length)]);
+  }
+}
+
+function createFirework(color) {
+  const firework = document.createElement("div");
+  firework.classList.add("firework");
+  firework.classList.add(color);
+  firework.style.left = Math.random() * window.innerWidth + "px";
+  firework.style.top = Math.random() * window.innerHeight + "px";
+  firework.style.animationDuration = Math.random() * 2 + 1 + "s";
+  document.getElementById("fireworks-container").appendChild(firework);
+
+  setTimeout(() => {
+    firework.style.transform = "scale(0)";
+  }, 100);
+}
+
+function typeWriterEffect(text, colors) {
+  const textElement = document.getElementById("typing-text");
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      textElement.innerHTML += `<span style="color: ${
+        colors[index % colors.length]
+      };">${text.charAt(index)}</span>`;
+      index++;
+      setTimeout(type, 50);
     }
+  }
+
+  type();
 }
-
-function createFirework() {
-    const firework = document.createElement('div');
-    firework.classList.add('firework');
-    firework.style.left = Math.random() * window.innerWidth + 'px';
-    firework.style.top = Math.random() * window.innerHeight + 'px'; /* Agregar un estilo 'top' aleatorio */
-    firework.style.animationDuration = (Math.random() * 2 + 1) + 's';
-    document.getElementById('fireworks-container').appendChild(firework);
-}
-
-
-
